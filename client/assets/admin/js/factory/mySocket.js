f63a20a794d7e4f14a78394b1af42e138e9f6eba
yudiz php <yudiz.games.com>
2017-02-24 19:19:52 +0530

    Exam Round 1 23-02-2017

--- client/assets/admin/js/factory/mySocket.js
+++ client/assets/admin/js/factory/mySocket.js
@@ -1,6 +1,6 @@
 angular.module('main').factory('mySocket', function (socketFactory) {
     return socketFactory({
-        // ioSocket: io.connect('http://158.69.227.67:4000')
-        ioSocket: io.connect('http://192.168.10.165:4000')
+        ioSocket: io.connect('http://158.69.227.67:4000')
+        // ioSocket: io.connect('http://192.168.10.165:4000')
     });
 });
